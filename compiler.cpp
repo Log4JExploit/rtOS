2afcdb1872897b8c4387a8400047ba0309e839ef
Log4JExploit <>
2024-08-26 11:10:02 +0200

    Started work on compiler and added example script

--- compiler.cpp
+++ compiler.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
+using namespace std;
+
+enum class BasicType {
+   Bool,
+   Byte,
+   Char,
+   Short,
+   Int,
+   Float,
+   Double,
+   Long,
+   String
+};
+
+struct ConstantValue {
+   BasicType type;
+   int sizeInBytes;
+   char *data;
+   int id;
+}
+
+struct Function {
+       
+};
+
+int valuesCounter;
+ConstantValue *values;
+
+
+int main(int argsCount, char **args) {
+   if(argsCount < 3) {
+      cerr << "Requires two arguments: 1: input file, 2: output file" << endl;
+      return 1;
+   }
    
+   string inputFileName(args[1]);
+   string outputFileName(args[2]);
+
+   ifstream inputFile(inputFileName);
+   ofstream outputFile(outputFileName); 
+
+   if(!inputFile.is_open()) {
+      return 2;
+   }
+
+   std::string inputContent;
+
+   while(inputFile) {
+       char c = inputFile.get();
+       if(c == '\r') {
+          continue;
+       }
+       if(c == '\n') {
+          c = ' ';
+       }
+       inputContent.push_back(c);
+   } 
+
+   cout << inputContent << endl;
+
+   inputFile.close();
+   outputFile.close();
+
    return 0;
 }
