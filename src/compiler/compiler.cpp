97e6d8e6ba3c31bb01d035ffeaa163ceb3996a6e
Log4JExploit <>
2024-08-29 22:50:14 +0200

    Added Tokenizer

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 enum class TokenType {
    Keyword,
-   Colon,
-   DoubleColon,
+   Digits,
+   Alpha,
+   
    BracketOpen,
    BracketClose,
    SquareBracketOpen,
@@ -18,38 +19,36 @@ enum class TokenType {
    BraceClose,
    AngleBracketOpen,
    AngleBracketClose,
-   Digits,
-   Alpha,
+   
    Plus,
    Minus,
    Slash,
    Star,
-   Hashtag,
+   
    QuoteSingle,
    QuoteDouble,
+   
+   Hashtag,
+   Colon,
    Dollar,
    Space,
    Point,
-   Other
+
+   And,
+   Or,
+   Equals,
+
+   Separator,
+   NewLine, 
+   Other,
+   EoF
 };
 
 const char *keywords[] = {
-	          "bool", "char", "byte", "long", "int", "double", "float", "short", "string",
+	          "bool", "char", "byte", "long", "int", "double", "float", "short", "string", "null"
                   "create", "store", "for", "delete", "if", "calc", "inc", "dec", "exit", "done",
                   "up", "down", "below", "above", "not", "invoke", "function", nullptr
 };
-
-const char *operators[] = {
-   "+", "-", "*", "/", nullptr
-};
-
-const char *brackets[] = {
-   "(", ")", "{", "}", "[", "]", nullptr
-};
-
-const char *separators[] = {
-   " ", "	", "\n", "\r"
-}
 
 struct Token {
    TokenType type;
@@ -88,19 +87,42 @@ struct Function {
    char *name;
 };
 
+Token endOfFileToken;
 Function *functions;
 int functionsCounter;
 
 Variable *constants;
 int constantsCounter;
 
+/* Functions */
+
+// TOKENIZER
+
 vector<Token> tokenize(char *text, int length);
+
 Token nextToken(char *text, int length);
+Token nextKeywordToken(char *text, int length);
+Token nextAlphabeticToken(char *text, int length);
+Token nextNumericToken(char *text, int length);
+Token nextSpecialToken(char *text, int length);
+
+// PARSER
+
+void verify(vector<Token> *tokens) {
+    
+}
+
+/* Util Functions */
+
 int arrayFind(const char *array[], char* text, int length);
+char* charcpy(const char *src, int length);
+
 bool isAlphabetic(char character);
 bool isUpper(char character);
 bool isLower(char character);
 bool isNumeric(char character);
+
+/* main */
 
 int main(int argsCount, char **args) {
    if(argsCount < 3) {
@@ -108,6 +130,8 @@ int main(int argsCount, char **args) {
       return 1;
    }
    
+   endOfFileToken.type = TokenType::EoF;
+
    string inputFileName(args[1]);
    string outputFileName(args[2]);
 
@@ -130,21 +154,24 @@ int main(int argsCount, char **args) {
    } 
  
    int length = inputContent.length(); 
-   const char *constantStr = inputContent.c_str(); 
-   char *content = static_cast<char*>(malloc(length));
-   memcpy(content, constantStr, length);
+   char *content = charcpy(inputContent.c_str(), length);
 
    cout << content << endl;
 
    inputFile.close();
    
    vector<Token> tokens = tokenize(content, length);
-   
+  
+   for(Token token : tokens) {
+      cout << "\"" << token.text << "\", ";
+   }
+
    outputFile.close();
 
    return 0;
 }
 
+/* tokenizer */
 
 vector<Token> tokenize(char *text, int length) {
    int index = 0;
@@ -164,54 +191,118 @@ vector<Token> tokenize(char *text, int length) {
 Token nextToken(char *text, int length) {
    char first = text[0];
 
+   if(first == 0 || length == 0) {
+      return endOfFileToken;
+   }
+
    if(isAlphabetic(first)) {
       if(isLower(first)) {
          return nextKeywordToken(text, length);
       } else {
-         return nextIdentifier(text, length);
+         return nextAlphabeticToken(text, length);
       }
    } else if(isNumeric(first)) {
-      nextNumericToken(text, length);
+      return nextNumericToken(text, length);
    }
 
-   int keywordLength = arrayFind(keywords, text);
+   return nextSpecialToken(text, length);
+}
 
-   if(keywordLength > 0) {
-      token.type = TokenType::Keyword;
-      token.length = keywordLength;
-      return token;
+Token nextSpecialToken(char *text, int length) {
+   char first = text[0];    
+  
+   Token token;
+   token.text = charcpy(text, 1);
+   token.length = 1;
+   
+   switch(text[0]) {
+      case '+': token.type = TokenType::Plus; break;
+      case '-': token.type = TokenType::Minus; break;
+      case '*': token.type = TokenType::Star; break;
+      case '/': token.type = TokenType::Slash; break;
+
+      case ' ': 
+      case '	': 
+      case '\r': token.type = TokenType::Separator; break;
+      
+      case '(': token.type = TokenType::BracketOpen; break;
+      case ')': token.type = TokenType::BracketClose; break;
+      case '{': token.type = TokenType::BraceOpen; break;
+      case '}': token.type = TokenType::BraceClose; break;
+      case '[': token.type = TokenType::SquareBracketOpen; break;
+      case ']': token.type = TokenType::SquareBracketClose; break;
+      
+      case '$': token.type = TokenType::Dollar; break;
+      case '#': token.type = TokenType::Hashtag; break;
+      case ':': token.type = TokenType::Colon; break;
+      case '.': token.type = TokenType::Point; break;
+      
+      case '\'': token.type = TokenType::QuoteSingle; break;
+      case '\"': token.type = TokenType::QuoteDouble; break;
+     
+      case '&': token.type = TokenType::And; break;
+      case '|': token.type = TokenType::Or; break;
+      case '=': token.type = TokenType::Equals; break;
+      
+      case '\n': token.type = TokenType::NewLine; break;
+
+      default:
+         token.type = TokenType::Other;
+	 break;
    }
 
-   int operatorLength = arrayFind(operators, text);
-
-   if(operatorLength > 0) {
-      switch(text[0]) {
-	      case '+': token.type = TokenType::Plus; break;
-	      case '-': token.type = TokenType::Minus; break;
-	      case '*': token.type = TokenType::Star; break;
-	      case '/': token.type = TokenType::Slash; break;
-              default:
-			cerr << "UNKNOWN OPERATOR" << endl;
-			exit(1);
-			break;
-      }
-      token.length = 1;
-      return token;
-   }
-
+   token.length = 1;
    return token;
 }
 
-Token nextKeywordToken(char *text, int length) {
-   int keywordLength = arrayFind(keywords, text);
-
-   if(keywordLength > 0) {
-      token.type = TokenType::Keyword;
-      token.length = keywordLength;
-      return token;
+Token nextAlphaOrNumToken(char *text, int length, bool numeric) {
+   int index = 0;
+   while(text[index] != 0 && index < length) {
+      char c = text[index];
+      if(numeric ? !isNumeric(c) : !isAlphabetic(c)) {
+         break;
+      }
+      index++;
    }
-   return nextIdentifierToken(text, length);
+
+   if(index == 0) {
+      return endOfFileToken;
+   }
+
+   Token token;
+   token.type = numeric ? TokenType::Digits : TokenType::Alpha;
+   token.text = charcpy(text, index);
+   token.length = index;
+   return token;
 }
+
+Token nextAlphabeticToken(char *text, int length) {
+   return nextAlphaOrNumToken(text, length, false);
+}
+
+Token nextNumericToken(char *text, int length) {
+   return nextAlphaOrNumToken(text, length, true);
+}
+
+Token nextKeywordToken(char *text, int length) {
+   int keywordLength = arrayFind(keywords, text, length);
+
+   if(keywordLength < 1) {
+      return nextAlphabeticToken(text, length);
+   }
+
+   Token token;
+   token.text = charcpy(text, keywordLength);
+   token.type = TokenType::Keyword;
+   token.length = keywordLength;
+   return token;
+}
+
+/* verify */
+
+
+
+/* util */
 
 int arrayFind(const char **array, char *text, int length) {
    for(int i = 0; array[i] != nullptr; i++) {
@@ -240,12 +331,20 @@ int arrayFind(const char **array, char *text, int length) {
    return 0;
 }
 
+
+char* charcpy(const char *src, int length) {
+   char *copy = static_cast<char*>(malloc(length + 1));
+   memcpy(copy, src, length);
+   copy[length] = 0;
+   return copy;
+}
+
 bool isAlphabetic(char c) {
    return isUpper(c) || isLower(c);
 }
 
 bool isUpper(char c) {
-   return (c > 191 && c < 223 ) || (c => 'A' && c <= 'Z'); 
+   return (c > 191 && c < 223 ) || (c >= 'A' && c <= 'Z'); 
 }
 
 bool isLower(char c) {
@@ -255,3 +354,5 @@ bool isLower(char c) {
 bool isNumeric(char c) {
    return c >= '0' && c <= '9';
 }
+
+/* todo */
